9390a9a17752a236f55b44eee6d4fcd58f923e04
zhong_zhangzhi <zhongzhangzhi@xiniaoyun.com>
2019-04-10 10:00:00 +0800

    initial commit

---
+++ src/main/java/com/example/dictionary/DictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictionaryApplication.class, args);
+    }
+
+}
